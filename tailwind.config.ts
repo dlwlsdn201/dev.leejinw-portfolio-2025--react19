103ba08bc73cc9afbc55c6f5bad7867d46052a3c
JinWoo_Lee <dlwlsdn201@naver.com>
2025-01-29 14:08:39 +0900

    :package: 애니메이션 효과 관련 패키지 설치 (AOS, motion)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import { tailwindThemeConfig } from 'src/app/config/colors';
+import { PADDING_CONFIG } from '@app/config/style';
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -6,13 +7,22 @@ export default {
   darkMode: 'class',
   theme: {
     extend: {
+      ...tailwindThemeConfig,
       colors: {
         // 이전에 공유한 컬러 팔레트 추가 가능
       },
       backgroundImage: {
         // 그라데이션 등 추가 가능
       },
-      ...tailwindThemeConfig,
+      animation: {
+        blink: 'blink 1s steps(2, start) infinite',
+      },
+      keyframes: {
+        blink: {
+          '0%, 100%': { opacity: 0 },
+          '50%': { opacity: 1 },
+        },
+      },
     },
   },
   plugins: [],
