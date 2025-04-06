656b779861c57fb28accf3226dbb84c18d69681f
JinWoo_Lee <dlwlsdn201@naver.com>
2025-04-06 15:41:35 +0900

    🏗️ 기존 client 기준 repo ->  server, client 구조 레포로 변경

--- src/features/comment/model/comment.ts
+++
@@ -1,9 +0,0 @@
-export interface NewGuestbookComment {
-  author: string;
-  password: string;
-  content: string;
-}
-export interface GuestbookComment extends NewGuestbookComment {
-  id: string;
-  createdAt: number; // unix
-}
