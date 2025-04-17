959245f320ca63fbefa02a43780df9b954ae1813
JinWoo_Lee <dlwlsdn201@naver.com>
2025-04-17 22:50:40 +0900

    :lipstick: #3 comment > `Notification` UI 동작 기능 구현

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import '@mantine/core/styles.css';
 import { Router } from './app/routes/routes';
 import { MantineProvider } from '@mantine/core';
+import { Notifications } from '@mantine/notifications';
+import './index.css';
+import '@mantine/core/styles.css';
+import '@mantine/notifications/styles.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <MantineProvider defaultColorScheme="dark">
+      <Notifications />
       <Router />
     </MantineProvider>
   </StrictMode>
